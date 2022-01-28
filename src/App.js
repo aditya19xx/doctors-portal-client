487ef67b18b14f5db497b4ec963b61c537d24178
aditya19xx <turjaadityadas@gmail.com>
2022-01-28 22:23:25 +0600

    appointment page setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
+import Appointment from "./Pages/Appointment/Appointment/Appointment";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />}/>
         <Route path="/home" element={<Home />} />
+        <Route path="/appointment" element={<Appointment />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -0,0 +1,21 @@
+import { Container, Grid } from "@mui/material";
+import React from "react";
+import chair from "../../../images/chair.png";
+import Calender from "../../Shared/Calender/Calender";
+
+const AppointmentHeader = () => {
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+            <Calender></Calender>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img style={{ width: "100%" }} src={chair} />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default AppointmentHeader;
